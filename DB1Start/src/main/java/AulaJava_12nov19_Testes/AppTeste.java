530def5e3b4812bc586b046874212d6890360e7f
joaopasquini <joaopasquinijr@hotmail.com>
2019-11-12 16:52:33 -0300

    Realizados alguns Testes Unitários dos exercicios do arquivo de anotações

--- DB1Start/src/main/java/AulaJava_12nov19_Testes/AppTeste.java
+++ DB1Start/src/main/java/AulaJava_12nov19_Testes/AppTeste.java
@@ -7,21 +7,66 @@ import org.junit.Test;
 public class AppTeste {
 
     @Test
-    public void teste() {
+    public void deveRetornar8() {
         Application application = new Application();
-        Integer expected = 3;
-        Integer response = application.soma(1,2);
+        Integer expected = 8;
+        Integer response = application.soma(5,3);
         Assert.assertEquals(expected, response);
     }
 
     @Test
-    public void teste2() {
+    public void naoDeveRetornar6() {
         Application application = new Application();
         Integer expected = 6;
-        Integer response = application.soma(3,3);
-        Assert.assertEquals(expected, response);
+        Integer response = application.soma(5,3);
+        Assert.assertNotEquals(expected, response);
     }
 
+    @Test
+    public void contarQuantidadeDeImparesDe10Ate100(){
+        Application application = new Application();
 
+        Integer expected = 50;
+
+        Integer response = application.numeroDeImpares(100);
+
+        Assert.assertEquals(expected,response);
+
+    }
+
+    @Test
+    public void contarQuantidadeDeImpares(){
+        Application application = new Application();
+
+        Integer expected = 0;
+
+        Integer response = application.numeroDeImpares(0);
+
+        Assert.assertEquals(expected,response);
+
+    }
+
+    @Test
+    public void contarQuantidadeDeCaracteresNaString(){
+        Application application = new Application();
+
+        Integer expected = 8;
+
+        Integer response = application.contarQuantidadeDeCaracteres("DB1START");
+
+        Assert.assertEquals(expected,response);
+
+    }
+
+    @Test
+    public void StringReversa(){
+        Application application = new Application();
+
+        String palavraEsperada = "oiratinu etset trats1bd";
+
+        Assert.assertEquals(application.inverterString("db1start teste unitario"),palavraEsperada);
+
+
+    }
 
 }
--- DB1Start/src/main/java/AulaJava_12nov19_Testes/Application.java
+++ DB1Start/src/main/java/AulaJava_12nov19_Testes/Application.java
@@ -3,7 +3,43 @@ package AulaJava_12nov19_Testes;
 public class Application {
 
     public Integer soma(Integer numero1, Integer numero2){
-        return 3;
+        return numero1+numero2;
     }
 
+    public Integer subtrair(Integer numero1, Integer numero2){
+        return numero1-numero2;
+    }
+
+    public Integer numeroDeImpares(int numeroInicial) {
+        int contador = 0;
+
+        if(numeroInicial > 0){
+            for (int i=numeroInicial ; i > 0 ; i--){
+                if(i%2 != 0) contador+=1;
+            }
+        }
+        else {
+            for (int i=numeroInicial ; i < 0 ; i++){
+                if(i%2 != 0) contador+=1;
+            }
+        }
+
+        return contador;
+    }
+
+    public Integer contarQuantidadeDeCaracteres(String palavra) {
+        Integer numeroDeCaracteres = palavra.length();
+        return numeroDeCaracteres;
+    }
+
+    public String inverterString(String palavra) {
+
+        String teste = "";
+
+        for (int contador = palavra.length()-1 ; contador >= 0 ; contador--){
+            teste += palavra.charAt(contador);
+        }
+
+        return teste;
+    }
 }
