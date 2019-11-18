46163d0ecd9fde1a0a64fcb721c98410f2fb591f
Alunos Unifamma <alunos@intranet.lan>
2019-11-18 13:55:29 -0300

    Adicionada solução do exericicio de Mapas e Listas no pacote de exercicios de Estrutura

--- DB1Start/src/main/java/AulaJava_14nov19_ExerciciosDeLista/App.java
+++ DB1Start/src/main/java/AulaJava_14nov19_ExerciciosDeLista/App.java
@@ -6,12 +6,14 @@ public class App {
    public static void main (String[] Args){
 
        List<String> coresQueEuMaisGosto = new ArrayList<>();
-
        coresQueEuMaisGosto = retornarCoresQueEuMaisGosto();
 
+       HashMap<Integer,List<Integer>> mapaParesEImpares = new HashMap<>();
+       List<Integer> numeros = new ArrayList<>();
 
+       numeros.addAll(Arrays.asList(0,1,2,3,4,5,6,7,8,9));
 
-
+       retornarMapaDeParesEImpares(numeros);
    }
 
     public static List<String> retornarCoresQueEuMaisGosto(){
@@ -58,7 +60,22 @@ public class App {
         System.out.println(listaOrdenada);
     }
 
+    public static HashMap<Integer,List<Integer>> retornarMapaDeParesEImpares(List<Integer> listaDeNumeros){
+       HashMap<Integer,List<Integer>> mapaComNumerosSeparados = new HashMap<>();
+       List<Integer> numerosPares = new ArrayList<>();
+       List<Integer> numerosImpares = new ArrayList<>();
+       List<Integer> auxiliar = new ArrayList<>();
 
+       mapaComNumerosSeparados.put(0,numerosPares);
+       mapaComNumerosSeparados.put(1,numerosImpares);
+
+       for(Integer g : listaDeNumeros){
+           auxiliar = mapaComNumerosSeparados.get(g%2);
+           auxiliar.add(g);
+       }
+
+       return mapaComNumerosSeparados;
+    }
 
 
 
